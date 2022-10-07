ee5843e771035f0da8f9a02f75127bd3b37a02c8
Iryna Ishchanka <28myhome@gmail.com>
2022-10-07 19:00:03 +0300

    feat: start js-part / add pop-up burger

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -1,1 +1,22 @@
 console.log('Hello world')
+
+// Pop-up burger-menu
+
+const btnBurger = document.querySelector('.burger-menu');
+const burgerMenu = document.querySelector('.popup-menu-all');
+const closeBurger = document.querySelector('.pop-close')
+
+btnBurger.addEventListener('click', function() {
+    burgerMenu.classList.add('on-popup-menu-all')
+})
+
+closeBurger.addEventListener('click', function() {
+    burgerMenu.classList.remove('on-popup-menu-all')
+})
+
+window.addEventListener('click', function(event) {
+    if (event.target == burgerMenu) {
+        burgerMenu.classList.remove('on-popup-menu-all');
+    }
+});
+
