6a531fe2d6abc3d1b6ce289b7f37de8c2a675854
Iryna Ishchanka <28myhome@gmail.com>
2022-12-07 00:41:14 +0300

    feat: customize configuration files

--- news/src/components/controller/appLoader.js
+++ news/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '9b78ecd2d9634e92baeb230e6f7f04aa', // получите свой ключ https://newsapi.org/
         });
     }
 }
