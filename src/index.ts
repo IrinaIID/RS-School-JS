37036f77d29dc2773a5506decbd79117d2bb1f9f
Iryna Ishchanka <28myhome@gmail.com>
2023-01-18 14:32:25 +0300

    feat: add sata file with car models

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,24 @@
 import './index.css'
-const carSvg = require('svg-inline-loader?classPrefix!./assets/car.svg');
-console.log(carSvg)
+import { CAR_BRANDS_MODELS } from './data/models'
+
+// const baseUrl = 'http://127.0.0.1:3000';
+// const path = {
+//   garage: '/garage',
+//   winners: '/winners',
+// }
+console.log(CAR_BRANDS_MODELS[0])
+
+// const carSvg = require('svg-inline-loader?classPrefix!./assets/car.svg');
 
 
 console.log('Hello World!');
 
-function crateCarSvg() {
-  const svgElem = document.createElementNS('http://www.w3.org/1999/xhtml', 'svg');
-  const useElem = document.createElementNS('http://www.w3.org/1999/xhtml', 'use');
-  useElem.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', `${carSvg}`);
-  svgElem.appendChild(useElem);
-  console.log('gsg')
-}
+// function crateCarSvg() {
+//   const svgElem = document.createElementNS('http://www.w3.org/1999/xhtml', 'svg');
+//   const useElem = document.createElementNS('http://www.w3.org/1999/xhtml', 'use');
+//   useElem.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', `${carSvg}`);
+//   svgElem.appendChild(useElem);
+//   console.log('gsg')
+// }
 
-crateCarSvg()
+// crateCarSvg()
